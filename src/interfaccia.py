562d198841c9a4804891a4cc7f51678d139142dd
Luca Tramonti <luca.tramonti@ocsnet.it>
2025-09-02 18:04:34 +0200

    utility stampa

--- src/interfaccia.py
+++ src/interfaccia.py
@@ -5,30 +5,40 @@ class Interfaccia:
     def __init__(self):
         pass
 
+
     def chiedi_dati_fanta_allenatore(self):
         nome_fanta_allenatore = input("Nome fanta allenatore: ")
         nome_squadra = input("Nome squadra: ")
         return nome_fanta_allenatore, nome_squadra
 
-    def seleziona_comando(self, scelte: list[str]) -> int:
-        comandi_validi = list(range(len(scelte) + 1))[1:]
-        print("Selezionare operazione")
-        utils.stampa_elenco_scelte(scelte)
-        comando = input("Comando:")
 
-        print("comando scelto:", comando)
+    def seleziona_comando(self, scelte: list[str], intestazione="") -> int:
+        comandi_validi = [str(i) for i in range(1, len(scelte) + 1)]
+        result = None
+        go = True
+        while go:
+            print(intestazione)
+            print("Selezionare operazione")
+            utils.stampa_elenco_scelte(scelte)
+            comando = input("Comando:")
+            if comando in comandi_validi:
+                result = int(comando)
+                go = False
+            else:
+                print("<<<Comando non valido, riprovare>>>")
+        return result
+
 
     def home(self):
+        intestazione = "*----------------------*" \
+                       "*--- SCHERMATA HOME ---*" \
+                       "*----------------------*"
         scelte = [
             "Visualizza tornei",
-            "Gestisci tornei"
+            "Gestisci tornei",
+            "Esci"
         ]
-        print("*----------------------*")
-        print("*--- SCHERMATA HOME ---*")
-        print("*----------------------*")
-        print("Selezionare operazione")
-        utils.stampa_elenco_scelte(scelte)
-        scelta = input("Comando:")
+        scelta = self.seleziona_comando(scelte, intestazione)
         print("comando scelto:", scelta)
 
 
