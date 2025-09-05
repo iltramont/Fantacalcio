c064b587ce1df5f3db2882591eb42f5026754eab
Luca Tramonti <luca.tramonti@ocsnet.it>
2025-09-05 19:16:22 +0200

    aggiunte finestre

--- src/finestre.py
+++ src/finestre.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 import utils
 
 class Finestra:
@@ -6,7 +8,8 @@ class Finestra:
         self.descrizione: str | None = None
         self.larghezza: int = 100
         self.allineamento_titolo: str = '^'
-        self.comandi: dict[str, str] | None = None
+        self.comandi: dict[str, str] = dict()
+        self.comando_chiusura: str = '0'
 
     def stampa_titolo(self):
         utils.stampa_titolo(self.titolo, self.larghezza, self.allineamento_titolo)
@@ -15,6 +18,9 @@ class Finestra:
         if self.descrizione is not None:
             print(self.descrizione)
 
+    def contenuto(self):
+        pass
+
     def seleziona_comando(self) -> str:
         if self.comandi is None:
             return ''
@@ -22,21 +28,24 @@ class Finestra:
         while True:
             self.stampa_titolo()
             self.stampa_descrizione()
+            self.contenuto()
             print("*--- Selezionare comando")
             utils.stampa_elenco_scelte(self.comandi)
             comando = input("--->").strip()
             if comando in comandi_validi:
                 return comando
             else:
-                print("<<<Comando non valido, riprovare>>>")
+                print("<<< Comando non valido, riprovare >>>")
 
     def agisci(self, comando: str):
         pass
 
     def execute(self):
-        comando = self.seleziona_comando()
-        self.agisci(comando)
-
+        while True:
+            comando = self.seleziona_comando()
+            if comando == self.comando_chiusura:
+                break
+            self.agisci(comando)
 
 
 class Home(Finestra):
@@ -45,7 +54,7 @@ class Home(Finestra):
         self.titolo = 'HOME'
         self.comandi = {
             '1': 'Gestisci campionati',
-            '0': 'Esci'
+            self.comando_chiusura: 'Esci'
         }
 
     def agisci(self, comando: str):
@@ -62,6 +71,44 @@ class GestisciCampionati(Finestra):
         self.comandi = {
             '1': 'Visualizza campionati',
             '2': 'Crea nuovo campionato',
-            '0': 'Esci',
+            self.comando_chiusura: 'Esci'
         }
 
+    def agisci(self, comando: str):
+        if comando == '2':
+            next_window = CreaNuovoCampionato()
+            next_window.execute()
+
+
+
+class CreaNuovoCampionato(Finestra):
+    def __init__(self):
+        super().__init__()
+        self.titolo = 'CREA NUOVO CAMPIONATO'
+        self.__nome_file_campionati = 'campionati.xlsx'
+        self.df_campionati = pd.DataFrame()
+
+    def crea_campionato(self, nome_campionato: str):
+        data_creazione = pd.Timestamp.now()
+        new_record = pd.DataFrame({
+            'Campionato': [nome_campionato],
+            'Data creazione': [data_creazione.strftime("%d/%m/%Y")],
+            'Ora Creazione': [data_creazione.strftime("%H:%M:%S")],
+        })
+        df = utils.leggi_file_excel(self.__nome_file_campionati)
+        df_campionati = pd.concat([df, new_record], ignore_index=True)
+        utils.salva_file_excel(df_campionati, self.__nome_file_campionati)
+        self.df_campionati = df_campionati
+
+
+    def execute(self):
+        nome_campionato = utils.chiedi_stringa('Nome Campionato')
+        print(f'*--- Creazione campionato "{nome_campionato}" in corso...')
+        conferma: bool = utils.conferma()
+        if conferma:
+            self.crea_campionato(nome_campionato)
+            print(f'*--- Campionato {nome_campionato} creato con successo.')
+            print('*--- Elenco campionati:')
+            print(self.df_campionati)
+        else:
+            print("Creazione campionato annullata")
--- src/utils.py
+++ src/utils.py
@@ -20,3 +20,35 @@ def stampa_titolo(titolo: str, larghezza: int = 100, allineamento: str = '^'):
         s = f"*---{titolo:-{allineamento}{larghezza-8}}---*"
     print(s)
 
+
+def chiedi_stringa(campo: str):
+    while True:
+        result = input(f"*--- {campo}:").strip()
+        if result != '':
+            return result
+        else:
+            print('<<< Valore nullo non consentito >>>')
+
+
+def conferma() -> bool:
+    while True:
+        x = input("Confermare? [S / N]:").strip().upper()
+        if x == 'S':
+            return True
+        elif x == 'N':
+            return False
+        else:
+            print('<<< Comando non valido, valori accettati "S" / "N" >>>')
+
+
+def leggi_file_excel(file_name: str) -> pd.DataFrame:
+    l = os.getcwd().split("\\")
+    path = "\\".join(l[:len(l) - 1]) + "\\data\\" + file_name
+    if os.path.exists(path):
+        return pd.read_excel(path)
+    return pd.DataFrame()
+
+def salva_file_excel(df: pd.DataFrame, file_name: str):
+    l = os.getcwd().split("\\")
+    path = "\\".join(l[:len(l) - 1]) + "\\data\\" + file_name
+    df.to_excel(path, index=False)
